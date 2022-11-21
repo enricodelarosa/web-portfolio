385bd6df72af25b4d39112a191334cf983496239
Enrico Emil Dela Rosa <enricodelarosa01@gmail.com>
2022-11-21 11:42:20 +0800

    Add modal monty hall vid and remove svg arrows on card buttons

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ videoElement
     const alert = document.getElementById('lower-power-alert');
     alert.style.display = 'flex';
 });
+
+
+const montyhallCloseBtnElement = document.getElementById('monty-close');
+const montyhallVideo = document.getElementById('monty-hall-video');
+
+const montyModal = document.getElementById('monty-modal');
+
+montyModal.addEventListener('click', () => {
+    montyhallVideo.play();
+});
+
+montyhallCloseBtnElement.addEventListener('click', () => {
+    montyhallVideo.pause();
+});
